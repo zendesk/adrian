a30f443b503ec87e71ebd5a48fbc535ccafc264f
sponee <kyle.sponheim@zendesk.com>
2021-12-22 10:02:28 -0800

    Revert "fix deprecation warnings"
    This reverts commit 8cc3df9b15b1ab7e69d86a36a8d45ea85daf6b28.

--- test/array_queue_test.rb
+++ test/array_queue_test.rb
@@ -3,15 +3,15 @@ require_relative 'test_helper'
 describe Adrian::ArrayQueue do
   it 'should allow construction with an array' do
     q = Adrian::ArrayQueue.new([1,2,3])
-    _(q.pop.value).must_equal 1
-    _(q.pop.value).must_equal 2
-    _(q.pop.value).must_equal 3
-    _(q.pop).must_be_nil
+    q.pop.value.must_equal 1
+    q.pop.value.must_equal 2
+    q.pop.value.must_equal 3
+    q.pop.must_be_nil
   end
 
   it 'should allow construction without an array' do
     q = Adrian::ArrayQueue.new
-    _(q.pop).must_be_nil
+    q.pop.must_be_nil
   end
 
   it 'should act as a queue' do
@@ -21,13 +21,13 @@ describe Adrian::ArrayQueue do
     q.push(2)
     q.push(3)
 
-    _(q.length).must_equal 3
+    q.length.must_equal 3
 
-    _(q.pop.value).must_equal 1
-    _(q.pop.value).must_equal 2
-    _(q.pop.value).must_equal 3
-    _(q.pop).must_be_nil
+    q.pop.value.must_equal 1
+    q.pop.value.must_equal 2
+    q.pop.value.must_equal 3
+    q.pop.must_be_nil
 
-    _(q.length).must_equal 0
+    q.length.must_equal 0
   end
 end
